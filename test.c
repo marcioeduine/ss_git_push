72b5ed9933683092c327d48a4df6ebf0eab5bfcd
Marcio Caquarta <mcaquart@c2r5s9.42luanda.com>
2026-01-17 18:56:08 +0100

    UPDATED FILE:
     - ss_git_push:
       • Too many arguments.
    
     - test.c:
       • add() function prototype added.
       • main() function added.
       • Just return zero.

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
-//SS_COMMIT: add() function prototype added.
+// SS_COMMIT: add() function prototype added.
 void	add(int &a, int &b);
 
-//SS_COMMIT: main() function added.
+// SS_COMMIT: main() function added.
 int	main(void)
 {
-	return (0); //SS_COMMIT: Just return 0.
+	return (0); // SS_COMMIT: Just return zero.
 }
