1e299409207aff51cba90013e599261c88085397
Marcio Caquarta <mcaquart@c2r5s9.42luanda.com>
2026-01-17 19:01:37 +0100

    UPDATED FILE:
     - ss_git_push:
       • Too many arguments.
    
     - test.c:
       • add() function deleted.

--- test.c
+++ test.c
@@ -1,8 +1,5 @@
-// SS_COMMIT: add() function prototype added.
-void	add(int &a, int &b);
-
-// SS_COMMIT: main() function added.
+// SS_COMMIT: add() function deleted.
 int	main(void)
 {
-	return (0); // SS_COMMIT: Just return zero.
+	return (0);
 }
